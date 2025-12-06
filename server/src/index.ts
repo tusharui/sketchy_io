57af7d268defead14a7550f7753c58a389f4b02e
Roshan A <roshananand2005@gmail.com>
2025-12-06 11:03:50 +0530

    staging : setup socket.io with bun server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,30 @@
-import { Hono } from "hono";
+import { Server as Engine } from "@socket.io/bun-engine";
+import { Server } from "socket.io";
 
-const app = new Hono();
+// Ensure the WEB_URL environment variable is set
+const url = Bun.env.WEB_URL;
+if (!url) {
+	console.error("Error: WEB_URL environment variable is required. Exiting.");
+	process.exit(1);
+}
 
-app.get("/", (c) => {
-	return c.text("Hello Hono!");
+const io = new Server();
+
+const engine = new Engine({
+	path: "/socket.io/",
+	cors: {
+		origin: url,
+	},
+	pingTimeout: 60000, // 60 seconds
 });
 
-export default app;
+io.bind(engine);
+
+io.on("connection", (socket) => {
+	console.log("new connection :", socket.id);
+});
+
+export default {
+	port: 3000,
+	...engine.handler(),
+};
